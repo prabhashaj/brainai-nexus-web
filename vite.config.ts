b224082ad2132ce0e8a6ae6fffa5eface4ceada8
prabhashaj <149933940+prabhashaj@users.noreply.github.com>
2025-05-06 17:44:55 +0530

    changed code implementation

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/brainai-nexus-web/',
+  base: process.env.VITE_BASE_PATH ||'/brainai-nexus-web/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
