7cf56742684af23175cf25b3dd435742f02b52c6
prabhashaj <149933940+prabhashaj@users.noreply.github.com>
2025-05-08 11:55:06 +0530

    updated root path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ||'/brainai-nexus-web/',
+  base: '/', // Update base path for Vercel deployment
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
